14fc5bca2811d639f8224aa51b8d857e6fcfb9c7
Than Than Soe <thanthansoe15151@gmail.com>
2023-07-01 10:55:20 +0630

    five coment

--- embedvideo/settings.py
+++ embedvideo/settings.py
@@ -64,13 +64,31 @@ WSGI_APPLICATION = 'embedvideo.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+DATABASES_URL=os.getenv('DATABASES_URL',None)
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if not DATABASES_URL:
+    
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+
+else:
+    DATABASES = {
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'OPTIONS':{'sslmode':'require'},
+        }
+    }
+
+
 
 
 # Password validation
