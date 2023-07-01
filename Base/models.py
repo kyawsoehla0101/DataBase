153bd9b0d7a03844887647b844932b229eb4c45f
Mrat Pann Khine <mpk@gmail.com>
2023-07-01 18:36:44 +0800

    helo

--- Base/models.py
+++ Base/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    name=models.CharField(max_length=100)
+    
+    def _str__(self):
+        return self.name
